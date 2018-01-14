6ff34c8a71294a61e39418ed628e1ad340782648
Flinn <flinn.dolman@gmail.com>
2018-01-14 10:45:32 +0000

    update marker.html to include js files

--- Webapp/static/static/js/blockdemo.js
+++ Webapp/static/static/js/blockdemo.js
@@ -96,7 +96,7 @@ function payDeposit(depositPrice){
 
     var core = TenancyContract.at('0x250be4dc2186e6fd7d060e7f4e78a233dadfcc8d');
 
-    core.pay({from:window.web3.eth.accounts[0], value: depositPrice, gas:400000}, function(err){
+    core.pay({from:window.web3.eth.accounts[0], value: window.web3.toWei(depositPrice, "ether"), gas:400000}, function(err){
     });
     console.log(tenantName + 'added to the list of tenants');
 
